583b6008a521e13afbb72a88c301465572891007
mshotskiy <sha10man3544@gmail.com>
2020-02-06 14:27:03 +0200

    complete

--- src/main/java/com/tntu/weather/api/controller/WeatherApiController.java
+++ src/main/java/com/tntu/weather/api/controller/WeatherApiController.java
@@ -1,13 +1,7 @@
 package com.tntu.weather.api.controller;
 
 import com.tntu.weather.api.dto.WeatherDto;
-import com.tntu.weather.api.model.DarkSkyJson;
-import com.tntu.weather.api.model.OpenWeatherJson;
-import com.tntu.weather.api.model.WeatherBitJson;
-import com.tntu.weather.api.service.impl.DarkSkyWeatherApiService;
-import com.tntu.weather.api.service.impl.OpenWeatherApiService;
 import com.tntu.weather.api.service.impl.WeatherApiServiceImpl;
-import com.tntu.weather.api.service.impl.WeatherBitApiService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,16 +15,10 @@ public class WeatherApiController {
         this.weatherService = weatherService;
 
     }
-
-
     @GetMapping
-    public WeatherDto getWeather(){
+    public WeatherDto getWeather() {
         System.out.println("sdadasasd");
         return weatherService.getLastWeather();
     }
 
-//    @GetMapping
-//    public WeatherDto getWeather(){
-//        return weatherService.getLastWeather();
-//    }
 }
